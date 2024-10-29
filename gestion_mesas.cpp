3775c86aa82cd97b2085eb9faf6675571501d55f
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2024-10-29 10:22:13 -0300

    Ingreso de mesas

--- gestion_mesas.cpp
+++ gestion_mesas.cpp
@@ -1,31 +1,61 @@
-#include<iostream>
-#include<cstring>
-#include<fstream>
+#include <iostream>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 
-struct Mesas {
-    int numero_mesa;
-    bool esta_libre;
-    int ganancias_acumuladas;
+struct Mesas
+{
+  int numero_mesa;
+  bool esta_libre;
+  int ganancias_acumuladas;
 };
 
-int main(){
-    Mesas mesa;
+bool estaLibre(int numero_mesa);
+
+int main()
+{
+  Mesas mesa;
+  system("pause");
 }
 
-void cargarMesa (Mesas mesa[] ){
-     Mesas mesa;
-     bool esta_libre = false;
-    FILE* archivo=fopen("archivo.dat", "rb");
-    if(archivo != NULL){
-      while (fread (&mesa, sizeof(Mesas),1,archivo)==1)
+
+Mesas mesa()
+{
+  Mesas mesa;
+  bool estaLibre;
+  int gananciasAcumuladas;
+
+  cout << "Ingrese el numero de la mesa: " << endl;
+  cin >> mesa.numero_mesa;
+}
+
+bool estaLibre(int numero_mesa)
+{
+  Mesas mesa;
+  bool libre = false;
+
+  FILE *archivo = fopen("mesas.dat", "rb");
+  if (archivo != NULL)
+  {
+    while (fread(&mesa, sizeof(Mesas), 1, archivo) == 1)
+    {
+      if (mesa.numero_mesa == numero_mesa)
       {
-        cout <<"Ingrese el numero de la mesa:"<<endl;
-        cin >> mesa->numero_mesa;
-        
+        libre = true;
+        break;
       }
-      
     }
-    
+    fclose(archivo);
+  }
+
+  if (libre)
+  {
+    cout << "La mesa numero " << mesa.numero_mesa << " esta ocupada. " << endl;
+  }
+  else
+  {
+    cout << "La mesa numero " << mesa.numero_mesa << " esta libre. " << endl;
+  }
+  return libre;
 }
